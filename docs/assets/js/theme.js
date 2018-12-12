9228023946261d079a9392f833726fb0af275af8
kanish671 <kanish671@yahoo.com>
2018-12-13 02:14:13 +0530

    ui for rating app

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -1,5 +1,117 @@
-$( document ).ready(function() {
+$(function() {
+	axios.defaults.baseURL = 'http://localhost:8080/service';
+	axios.defaults.timeout = 3000;
 
-  // Get started!
-
+	getAllTeams();
 });
+
+function getAllTeams() {
+	axios.get('/team/getall')
+		.then(function(response) {
+			console.log(response);
+			renderAllTeams(response.data.responseObj.teams);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+}
+
+function renderAllTeams(teams) {
+	_.each(teams, function(team) {
+		$('.team-selector-dropdown').append(`<button class="dropdown-item" id="team-${team.teamId}" data-teamid="${team.teamId}" type="button">${team.name}</button>`);
+	});
+	bindAllTeamActions();
+}
+
+function renderAllLeagues(leagues) {
+	_.each(leagues, function(league) {
+		$('.league-selector-dropdown').append(`<button class="dropdown-item" id="league-${league.leagueId}" data-leagueid="${league.leagueId}" type="button">${league.name}</button>`);
+	});
+	bindAllLeagueActions();
+}
+
+function bindAllTeamActions() {
+	$('.team-selector-dropdown .dropdown-item').on('click', function(e) {
+		console.log('clicked');
+		var teamId = $(e.currentTarget).attr('data-teamid');
+		$('.team-selector-button').text($(e.currentTarget).text());
+		$('.team-selector-button').attr('data-teamid', teamId);
+		$('.league-selector-div').removeClass('hide');
+		getAllLeaguesForTeam(teamId);
+	});
+}
+
+function bindAllLeagueActions() {
+	$('.league-selector-dropdown .dropdown-item').on('click', function(e) {
+		console.log('clicked');
+		var leagueId = $(e.currentTarget).attr('data-leagueid');
+		var teamId = $('.team-selector-button').attr('data-teamid');
+		$('.league-selector-button').text($(e.currentTarget).text());
+		$('.league-selector-button').attr('data-leagueid', leagueId);
+		getAllMatchesForTeam(teamId, leagueId, 1); // seasonId is 1 for now
+	});
+}
+
+function getAllLeaguesForTeam(teamId) {
+	axios.get('/league/getallbyfilters', {
+			params: {
+				teamId: teamId
+			}
+		})
+		.then(function(response) {
+			console.log(response);
+			renderAllLeagues(response.data.responseObj.leagues);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+}
+
+function getAllMatchesForTeam(teamId, leagueId, seasonId) {
+	axios.get('/match/getallbyfilters', {
+			params: {
+				teamId: teamId,
+				leagueId: leagueId,
+				seasonId: seasonId
+			}
+		})
+		.then(function(response) {
+			console.log(response);
+			renderChart();
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+}
+
+function renderChart() {
+	var chartData = {
+		labels: ["S", "M", "T", "W", "T", "F", "S"],
+		datasets: [{
+			data: [589, 445, 483, 503, 689, 692, 634],
+		},
+		{
+			data: [639, 465, 493, 478, 589, 632, 674],
+		}]
+	};
+
+	var chLine = document.getElementById("chart-line");
+	if (chLine) {
+		new Chart(chLine, {
+		type: 'line',
+		data: chartData,
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: false
+					}
+				}]
+			},
+			legend: {
+				display: false
+			}
+		}
+		});
+	}
+}
--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,10 @@ gulp.task("concatScripts", function() {
     return gulp.src([
         'assets/js/vendor/jquery-3.3.1.min.js',
         'assets/js/vendor/popper.min.js',
-        'assets/js/vendor/bootstrap.min.js'
+        'assets/js/vendor/bootstrap.min.js',
+        'assets/js/vendor/underscore-min.js',
+        'assets/js/vendor/axios.min.js',
+        'assets/js/vendor/chart.min.js'
         ])
     .pipe(maps.init())
     .pipe(concat('main.js'))
@@ -25,12 +28,12 @@ gulp.task("concatScripts", function() {
     .pipe(browserSync.stream());
 });
 
-gulp.task("minifyScripts", ["concatScripts"], function() {
+gulp.task("minifyScripts", gulp.series('concatScripts', function() {
   return gulp.src("assets/js/main.js")
     .pipe(uglify())
     .pipe(rename('main.min.js'))
     .pipe(gulp.dest('docs/assets/js'));
-});
+}));
 
 gulp.task('compileSass', function() {
   return gulp.src("assets/css/main.scss")
@@ -42,16 +45,16 @@ gulp.task('compileSass', function() {
       .pipe(browserSync.stream());
 });
 
-gulp.task("minifyCss", ["compileSass"], function() {
+gulp.task("minifyCss", gulp.series('compileSass', function() {
   return gulp.src("assets/css/main.css")
     .pipe(cssmin())
     .pipe(rename('main.min.css'))
     .pipe(gulp.dest('docs/assets/css'));
-});
+}));
 
 gulp.task('watchFiles', function() {
-  gulp.watch('assets/css/**/*.scss', ['compileSass']);
-  gulp.watch('assets/js/*.js', ['concatScripts']);
+  gulp.watch('assets/css/**/*.scss', gulp.series('compileSass'));
+  gulp.watch('assets/js/*.js', gulp.series('concatScripts'));
 })
 
 gulp.task('browser-sync', function() {
@@ -62,8 +65,9 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('clean', function() {
+gulp.task('clean', function(done) {
   del(['docs', 'assets/css/main.css*', 'assets/js/main*.js*']);
+  done();
 });
 
 gulp.task('renameSources', function() {
@@ -75,21 +79,19 @@ gulp.task('renameSources', function() {
     .pipe(gulp.dest('docs/'));
 });
 
-gulp.task("build", ['minifyScripts', 'minifyCss'], function() {
-  return gulp.src(['*.html', '*.php','*.css','favicon.ico',
-                   "assets/img/**","assets/css/theme.css","assets/js/theme.js","assets/fonts/**"], { base: './'})
+gulp.task("build", gulp.series('minifyScripts', 'minifyCss', function() {
+  return gulp.src(['*.html', '*.php','*.css',
+                   "assets/img/**","assets/css/theme.css","assets/js/theme.js","assets/font/**"], { base: './'})
             .pipe(gulp.dest('docs'));
-});
+}));
 
-gulp.task('serve', ['watchFiles'], function(){
+gulp.task('serve', function(){
   browserSync.init({
         server: "./"
     });
 
-    gulp.watch("assets/css/**/*.scss", ['watchFiles']);
+    gulp.watch("assets/css/**/*.scss", gulp.parallel('watchFiles'));
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-gulp.task("default", ["clean", 'build'], function() {
-  gulp.start('renameSources');
-});
+gulp.task("default", gulp.series("clean", 'build'));
